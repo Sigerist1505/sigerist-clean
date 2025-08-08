a69376b292bbe9db9f23a8b3013472ae65dc8573
Sigerist <Daniel.sigerist101@gmail.com>
2025-08-08 04:22:11 +0200

    correcion .env

--- seed-database.js
+++ seed-database.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 // Script para crear tablas y datos iniciales en Neon Database
+import 'dotenv/config';
 import { neon } from '@neondatabase/serverless';
 const sql = neon(process.env.DATABASE_URL);
 
